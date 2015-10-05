7fd3664cb460d39752df5201ec804ca8c0558c80
Karen McCann <kare.mc@live.co.uk>
2015-10-05 17:26:25 +0100

    jquery/ javascript

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function()
+{
+	alert('Hello World');
+});
+
+  /***********************************
+  UNIQUE FUNCTIONS
+  ***********************************/
+
+$( "#clickme" ).click(function() {
+  $( "#book" ).slideToggle( "slow", function() {
+    // Animation complete.
+  });
+});
+
+				
+
+
